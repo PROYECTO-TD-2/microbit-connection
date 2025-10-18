fcee4c4e426bfa4a0a8be458ec5a2f34ca3be9bd
Fabio <fabiofernandez.it@gmail.com>
2025-10-18 14:26:08 -0300

    correccion de nombre

--- alumno/clock.py
+++ alumno/clock.py
@@ -2,7 +2,7 @@ from microbit import *
 
 # Lista de imágenes del reloj
 clock_images = [
-    Image.CLOCK11, Image.CLOCK10, Image.CLOCK9, Image.CLOCK8, Image.CLOCK7,
+    Image.CLOCK12,Image.CLOCK11, Image.CLOCK10, Image.CLOCK9, Image.CLOCK8, Image.CLOCK7,
     Image.CLOCK6, Image.CLOCK5, Image.CLOCK4, Image.CLOCK3, Image.CLOCK2, Image.CLOCK1
 ]
 
